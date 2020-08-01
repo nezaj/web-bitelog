669b9a241aecb296a314fd2e92ab25753824bccf
Joe Averbukh <joeaverbukh@gmail.com>
2020-08-01 04:39:07 -0400

    Update health data

--- health.js
+++ health.js
@@ -7,16 +7,12 @@
 const fs = require("fs");
 const path = require("path");
 
-// const {
-//   HEALTH_BODY_MASS_KEY,
-//   HEALTH_WATER_KEY,
-// } = require("./src/constants.js");
+const {
+  HEALTH_BODY_MASS_KEY,
+  HEALTH_WATER_KEY,
+} = require("./src/constants.js");
 
-const BODY_MASS_KEY = "HKQuantityTypeIdentifierBodyMass";
-const WATER_KEY = "HKQuantityTypeIdentifierDietaryWater";
-const healthKeys = new Set([BODY_MASS_KEY, WATER_KEY]);
-
-// const healthKeys = new Set([HEALTH_BODY_MASS_KEY, HEALTH_WATER_KEY]);
+const healthKeys = new Set([HEALTH_BODY_MASS_KEY, HEALTH_WATER_KEY]);
 
 try {
   const dataPath = path.resolve(__dirname, "src", "data", "temp_health.json");
@@ -24,20 +20,7 @@ try {
   const filteredData = Object.keys(healthData)
     .filter((key) => healthKeys.has(key))
     .reduce((healthMap, key) => {
-      let refinedKey;
-      switch (key) {
-        case BODY_MASS_KEY:
-          refinedKey = "bodyMass";
-          break;
-        case WATER_KEY:
-          refinedKey = "water";
-          break;
-        // Unsupported key
-        default:
-          return healthMap;
-      }
-
-      healthMap[refinedKey] = healthData[key];
+      healthMap[key] = healthData[key];
       return healthMap;
     }, {});
 
--- src/App.js
+++ src/App.js
@@ -204,12 +204,11 @@ const Entry = ({ ds, items, detailMap, notes, healthItems, onShowDetail }) => {
   };
 
   const healthLabels = {
-    water: 1,
-    // water:
-    //   healthItems &&
-    //   healthItems[HEALTH_WATER_KEY] &&
-    //   round(healthItems[HEALTH_WATER_KEY], 2),
-    bodyMass: healthItems && healthItems.bodyMass,
+    water:
+      healthItems &&
+      healthItems[HEALTH_WATER_KEY] &&
+      round(healthItems[HEALTH_WATER_KEY], 2),
+    bodyMass: healthItems && healthItems[HEALTH_BODY_MASS_KEY],
   };
 
   const entryDate = friendlyDate(ds);
